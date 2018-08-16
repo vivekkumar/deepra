7b8489503b20b38939a23631272e7401eafb2ad4
Vivek Kumar <vivekkumarbt@gmail.com>
2018-08-17 00:27:16 +0530

    Update server.js

--- services/server.js
+++ services/server.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+
+const username = "vivekkumarbt";
+const password = "bluedog123";
+const dbUrl = `mongodb://${username}:${password}@ds217002.mlab.com:17002/dpradb`;
+mongoose.connect('dbUrl');
+
+
+// Get Mongoose to use the global promise library
+mongoose.Promise = global.Promise;
+//Get the default connection
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on('open', () => {
+	console.log("DBRA Opned!!");
+})
 
 app.get('/', (req, res) => {
   res.send('Hello from App Engine!');
 });
-app.get('/hello', (req, res) => {
-  res.send('Hello Again from App Engine!');
-});
+
 // Listen to the App Engine-specified port, or 8080 otherwise
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
